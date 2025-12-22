56a3644350dd12ba9ecc030b721d88272f8b62d9
Lauan097 <lauan.campos64@gmail.com>
2025-12-21 23:24:08 -0300

    Update v1.3.11

--- utils/wsHandlers.js
+++ utils/wsHandlers.js
@@ -211,6 +211,23 @@ registerHandler('send_embed', async ({ client, payload }) => {
   }
 });
 
+registerHandler('get_status', async ({ client }) => {
+  const lavalinkNodes = client.manager && client.manager.shoukaku ? client.manager.shoukaku.nodes : new Map();
+  const lavalinkStatus = Array.from(lavalinkNodes.values()).map(node => ({
+    name: node.name,
+    state: node.state,
+    stats: node.stats,
+    ping: node.ping || -1
+  }));
+
+  return {
+    uptime: process.uptime(),
+    ping: client.ws.ping,
+    memory: process.memoryUsage().rss,
+    lavalink: lavalinkStatus
+  };
+});
+
 module.exports = {
   registerHandler,
   executeHandler,
